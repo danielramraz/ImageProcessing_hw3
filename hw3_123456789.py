7aa557e87b44e07f43e000c629a00b38ecfe89ca
danielramraz <79479383+danielramraz@users.noreply.github.com>
2022-12-22 10:54:03 +0200

    sp noise done

--- hw3_123456789.py
+++ hw3_123456789.py
@@ -2,27 +2,81 @@ import numpy as np
 import cv2
 import random
 import matplotlib.pyplot as plt
-from scipy.signal import convolve2d
+# from scipy.signal import convolve2d
 
 # the copy in the first lines of the function is so that you don't ruin
 # the original image. it will create a new one. 
 
 def add_SP_noise(im, p):
     sp_noise_im = im.copy()
+    n,m =sp_noise_im.shape
+    size = n * m
+    # print("size = ", size)
+    length = len(sp_noise_im)
+    # print("length =", length)
+    
+    sp_noise_im = np.reshape(sp_noise_im, size, order='F')
+    # print("sp noise  =",sp_noise_im)
+    # print("sp length =",len(sp_noise_im))
 
+    noise_pixels = int(size * p)
+    # print("noise pixels = ", noise_pixels)
+    noise_values = [0, 255]
+
+    arr = random.sample( range(size) , noise_pixels)
+    # print("arr =", arr)
+    # print("sp noise shape =", sp_noise_im.shape)
+    # print(sp_noise_im)
+
+    for i in arr:
+        # print("i type of = ", type(i))
+        sp_noise_im[i] = random.choice(noise_values)
+
+    sp_noise_im = np.reshape(sp_noise_im, (n, m) , order='F')
+    # print("return image = ")
+    # print(sp_noise_im)
     return sp_noise_im
 
 
-def clean_SP_noise_single(im, radius):
+def clean_SP_noise_single(im, radius):              # median
     noise_im = im.copy()
-    # TODO: add implementation
+    # print("clean_SP_noise_single -> orig:\n", noise_im)
+
+    noise_im= np.pad(noise_im, pad_width = radius, mode = 'constant')
+    # print("clean_SP_noise_single -> added padding:\n", noise_im)
+    clean_im = noise_im
+    median_arr_size = radius * 2 + 1
+    # print("median_arr_size = ", median_arr_size)
+
+    for row in range(radius, len(noise_im)-radius):
+        for culomn in range(radius, len(noise_im[row])-radius):
+            if noise_im[row][culomn] == 0 or noise_im[row][culomn] == 255:
+                median_arr = (noise_im[(row - radius):(row - radius + median_arr_size), (culomn - radius):(culomn - radius + median_arr_size)])
+                # print("median arr: \n", median_arr)
+                median_val = np.median(median_arr)
+                # print("median val: \n", median_val)
+                clean_im[row][culomn] = median_val
+            
+
+    clean_im = delete_padding(clean_im, radius)
+    # print("clean_SP_noise_single -> cleaned:\n", clean_im)
 
     return clean_im
 
+def delete_padding(image, padding):
+    for i in range(padding):
+        n,m = image.shape
+        image = np.delete(image, [0,m-1], axis = 1)       #column
+        image = np.delete(image, [0,n-1], axis = 0)       #row
+
+    # print("clean_SP_noise_single -> delete_padding:\n", image)    
+    return image
 
 def clean_SP_noise_multiple(images):
-    # TODO: add implementation
-    return clean_image
+    # clean_images = images
+    # for image in clean_images:
+    #     clean_images = clean_SP_noise_single(image)
+    return #clean_images
 
 
 def add_Gaussian_Noise(im, s):
--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 # %%
-
 from hw3_123456789 import *
-
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     im = cv2.imread(r'Images\lena.tif')
     im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-
 
     # Salt and pepper noise ----------------------------------------------------------------
     n = 50
@@ -37,22 +33,22 @@ if __name__ == '__main__':
 
 
     # Gaussian noise ----------------------------------------------------------------
-    STD=15
-    gaussian_noised_im = add_Gaussian_Noise(im, s=STD)
-    clean_im = clean_Gaussian_noise(gaussian_noised_im, 2, 5)
-    clean_bi_im = clean_Gaussian_noise_bilateral(gaussian_noised_im, 1, 5, 25)
+    # STD=15
+    # gaussian_noised_im = add_Gaussian_Noise(im, s=STD)
+    # clean_im = clean_Gaussian_noise(gaussian_noised_im, 2, 5)
+    # clean_bi_im = clean_Gaussian_noise_bilateral(gaussian_noised_im, 1, 5, 25)
 
     # plot
-    plt.figure()
-    plt.subplot(1, 3, 1)
-    plt.imshow(gaussian_noised_im, cmap='gray', vmin=0, vmax=255)
-    plt.title('image + Gaussian noise')
-    plt.subplot(1, 3, 2)
-    plt.imshow(clean_im, cmap='gray', vmin=0, vmax=255)
-    plt.title('cleaned gaussian image')
-    plt.subplot(1, 3, 3)
-    plt.imshow(clean_bi_im, cmap='gray', vmin=0, vmax=255)
-    plt.title('Cleaned using bilateral')
+    # plt.figure()
+    # plt.subplot(1, 3, 1)
+    # plt.imshow(gaussian_noised_im, cmap='gray', vmin=0, vmax=255)
+    # plt.title('image + Gaussian noise')
+    # plt.subplot(1, 3, 2)
+    # plt.imshow(clean_im, cmap='gray', vmin=0, vmax=255)
+    # plt.title('cleaned gaussian image')
+    # plt.subplot(1, 3, 3)
+    # plt.imshow(clean_bi_im, cmap='gray', vmin=0, vmax=255)
+    # plt.title('Cleaned using bilateral')
 
-    plt.show()
+    # plt.show()
     # %%
